afa37b9f335c58277e0d76dd51bd803267c2c0e4
sa2taka <sa2taka@gmail.com>
2017-04-23 18:39:38 +0900

    First commit

---
+++ CenozoicForMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CenozoicForMac
+//
+//  Created by 薩田和弘 on 2017/04/23.
+//  Copyright © 2017年 Kazuhiro S. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ CenozoicForMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CenozoicForMac
+//
+//  Created by 薩田和弘 on 2017/04/23.
+//  Copyright © 2017年 Kazuhiro S. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
