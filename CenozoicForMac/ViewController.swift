d279bf826645af7a6c9511db7343b2bb93c1b8b0
sa2taka <sa2taka@gmail.com>
2017-04-24 21:07:37 +0900

    修正: JSONがからデータを送ってきても大丈夫な状態に修正(ただしまだ潜在的なバグはある可能性が)

--- CenozoicForMac/ViewController.swift
+++ CenozoicForMac/ViewController.swift
@@ -14,10 +14,10 @@ class ViewController: NSViewController {
   var isSpeaking = false
   let timelineURL = "https://mstdn-workers.com/api/v1/timelines/public?local=true"
   var lastID = 0;
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-
+    
     // Do any additional setup after loading the view.
     
     initSpeaker()
@@ -26,15 +26,15 @@ class ViewController: NSViewController {
     
     Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(self.onUpdate(_:)), userInfo: nil, repeats: false)
   }
-
+  
   override var representedObject: Any? {
     didSet {
-    // Update the view, if already loaded.
+      // Update the view, if already loaded.
     }
   }
   
   func initSpeaker(){
-    speaker.rate *= 2
+    speaker.rate *= 3
   }
   
   func onUpdate(_ timer: Timer){
@@ -52,11 +52,13 @@ class ViewController: NSViewController {
       if let jsonData = data {
         // ここで取得したデータからJSONを抜き出し喋らせる処理を行う
         let json = self.getJSON(jsonData)
-        var speakedContents = self.getSpeakedContents(json)
-        self.updateLastID(json)
-        speakedContents = speakedContents.reversed() // 逆順に取得したコンテンツを逆さまにして時系列に変更する
-        for content in speakedContents{
-          self.speakContent(content)
+        if(json.count != 0){
+          var speakedContents = self.getSpeakedContents(json)
+          self.updateLastID(json)
+          speakedContents = speakedContents.reversed() // 逆順に取得したコンテンツを逆さまにして時系列に変更する
+          for content in speakedContents{
+            self.speakContent(content)
+          }
         }
       }
       self.isSpeaking = false
@@ -68,7 +70,13 @@ class ViewController: NSViewController {
   func getJSON(_ data: Data) -> NSArray{
     var json : NSArray = []
     do{
-      json = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! NSArray
+      // FIXME ここでdataが空のjson受け取るとisEmptyはfalseだがNSArrayでのキャストで落ちる
+      if(data.count > 5){
+        json = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! NSArray
+      }
+      else {
+        json = NSArray()
+      }
     }
     catch{
       print("error: getJSON()")
@@ -76,11 +84,13 @@ class ViewController: NSViewController {
     return json
   }
   
-  // JSONからコンテント(HTMLタグを削除した)を抜き出してArrayとして返す
+  // JSONからコンテント(HTMLタグを削除し、URLを省略したもの)を抜き出してArrayとして返す
   func getSpeakedContents(_ json: NSArray) -> Array<String> {
     var speakedContents = Array<String>()
     for data in json {
-      speakedContents.append(removeHTMLTag(str: String(describing: (data as! Dictionary<String, Any>)["content"]!)))
+      var content = String(describing: (data as! Dictionary<String, Any>)["content"]!)
+      content = removeHTMLTag(str: content)
+      speakedContents.append(content)
     }
     return speakedContents
   }
