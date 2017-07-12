a098a5982780709bd089b97fdcb735ae48146f2c
sa2taka <sa2taka@gmail.com>
2017-07-13 00:08:29 +0900

    追加:文字列を改行分割したArrayを返してくれるものの追加

--- CenozoicForMac/ExtinctStringClass.swift
+++ CenozoicForMac/ExtinctStringClass.swift
@@ -6,11 +6,12 @@
 //  Copyright © 2017年 Kazuhiro S. All rights reserved.
 //
 
-// このソースはhttp://qiita.com/KikurageChan/items/807e84e3fa68bb9c4de6のソースコードより
-// 置換部のみをコピーしてきたものです
 import Foundation
 
 extension String {
+  // このソースはhttp://qiita.com/KikurageChan/items/807e84e3fa68bb9c4de6のソースコードより
+  // 置換部のみをコピーしてきたものです
+  
   //絵文字など(2文字分)も含めた文字数を返します
   var count: Int {
     let string_NS = self as NSString
@@ -21,5 +22,15 @@ extension String {
   func pregReplace(pattern: String, with: String, options: NSRegularExpression.Options = []) -> String {
     let regex = try! NSRegularExpression(pattern: pattern, options: options)
     return regex.stringByReplacingMatches(in: self, options: [], range: NSMakeRange(0, self.count), withTemplate: with)
+  }
+  
+  // ここからのソースはhttp://qiita.com/codelynx/items/183b29c95110e0dc05dbのソースコードです
+  // Stringを改行で分割したArrayに変換してくれるものです
+  var lines: [String] {
+    var lines = [String]()
+    self.enumerateLines { (line, stop) -> () in
+      lines.append(line)
+    }
+    return lines
   }
 }
